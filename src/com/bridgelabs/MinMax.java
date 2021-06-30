312ccaf963f95df38f49721a842d02400973a8d3
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-30 17:51:12 +0530

    Added: Method to check max float

--- src/com/bridgelabs/MinMax.java
+++ src/com/bridgelabs/MinMax.java
@@ -8,9 +8,17 @@ public class MinMax {
 	public static void main(String[] args) {
 	      Integer[] numbers = { 8, 2, 7};
 	      Integer maxint = (int) Collections.max(Arrays.asList(numbers));
-	     
+	      Float[] num = {2.3f, 4.7f, 1.1f};
+	      Float maxfloat = (float) Collections.max(Arrays.asList(num));
+
+	      toPrint(maxfloat);	     
 	      toPrint(maxint);
 	   }
+	private static void toPrint(Float maxfloat) {
+		System.out.println(maxfloat);
+		
+	}
+
 	private static void toPrint(Integer maxint) {
 		System.out.println(maxint);
 		
