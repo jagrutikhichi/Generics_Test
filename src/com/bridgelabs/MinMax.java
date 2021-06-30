2ef16957626f35d70613b3a3e4b1c25fa64df0e1
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-30 17:48:24 +0530

    Added: Method to check max integer

--- src/com/bridgelabs/MinMax.java
+++ src/com/bridgelabs/MinMax.java
@@ -1,5 +1,19 @@
 package com.bridgelabs;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MinMax {
+	
+	public static void main(String[] args) {
+	      Integer[] numbers = { 8, 2, 7};
+	      Integer maxint = (int) Collections.max(Arrays.asList(numbers));
+	     
+	      toPrint(maxint);
+	   }
+	private static void toPrint(Integer maxint) {
+		System.out.println(maxint);
+		
+	}
 
 }
